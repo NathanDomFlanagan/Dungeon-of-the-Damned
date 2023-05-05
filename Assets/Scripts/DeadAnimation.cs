9f93f3f36cd5df2247572095daeef6cd5e09c946
lucae3101 <114625986+lucae3101@users.noreply.github.com>
2023-05-05 13:02:18 +1200

    Destroy Wooden Box Added
    Added a damage to the box and a destroy animation for when the box is damaged.

---
+++ Assets/Scripts/DeadAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadAnimation : MonoBehaviour
+{
+    public Damageable dmg;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dmg = GetComponent<Damageable>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dmg.IsAlive)
+        {
+            animator.SetBool("IsAlive", false);
+            
+        }
+    }
+    void deathAnimationFinished()
+    {
+        GetComponent<Collider2D>().enabled = false; //Disables enemy collision
+        this.enabled = false; //Kills off the enemy
+        Destroy(gameObject, 0);
+    }
+}
