15dd9e5abad090ca3a88ad890852d28a81c4e409
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-12 18:29:32 +1200

    commiting cause unity freezingf

--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -9,6 +9,9 @@ public class ProjectileLauncher : MonoBehaviour
 
     public void fireProjectile()
     {
-        Instantiate(projectilePrefab, firePoint.position, projectilePrefab.transform.rotation);
+        GameObject proj = Instantiate(projectilePrefab, firePoint.position, projectilePrefab.transform.rotation);
+        Vector3 origScale = proj.transform.localScale;
+        
+        proj.transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1,1,1));
     }
 }
