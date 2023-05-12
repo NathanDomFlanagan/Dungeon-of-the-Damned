865d4c2f66ab453792566d190f575fa98bd06b88
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-12 19:19:28 +1200

    Projectile finally fixed

--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -7,18 +7,12 @@ public class ProjectileLauncher : MonoBehaviour
     public Transform firePoint;
     public GameObject projectilePrefab;
 
+
     public void fireProjectile()
     {
         GameObject proj = Instantiate(projectilePrefab, firePoint.position, projectilePrefab.transform.rotation);
         Vector3 origScale = proj.transform.localScale;
-        
-        if(transform.localRotation.eulerAngles.y == -180)
-        {
-            proj.transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
-        } else
-        {
-            proj.transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1, 1, 1));
-        }
+        proj.transform.localScale = new Vector3(origScale.x * transform.localScale.x > 0 ? 1 : -1, origScale.y, origScale.z);
 
     }
 }
