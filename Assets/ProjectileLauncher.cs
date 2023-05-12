04269fb3a232f014a3a0ade49409d60277b07645
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-12 17:00:43 +1200

    Still trying to fix boy direction

--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -11,8 +11,17 @@ public class ProjectileLauncher : MonoBehaviour
     {
         GameObject proj = Instantiate(projectilePrefab, firePoint.position, projectilePrefab.transform.rotation);
         Vector3 origScale = proj.transform.localScale;
+        int val = 1;
 
+        if (firePoint.position.x > 0)
+        {
+            val = 1;
+        }
+        if(firePoint.position.x < 0)
+        {
+            val = -1;
+        }
         //Flips the projectile depending on the direction the user's facing.
-        proj.transform.localScale = new Vector3(origScale.x * transform.localScale.x > 0 ? 1 : -1, origScale.y, origScale.z);
+        proj.transform.localScale = new Vector3(origScale.x * val, origScale.y, origScale.z);
     }
 }
