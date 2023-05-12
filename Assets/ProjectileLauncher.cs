23e749015842bdbcc0d9e117a0e0b8c85b80b667
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-12 17:48:51 +1200

    d

--- Assets/ProjectileLauncher.cs
+++ Assets/ProjectileLauncher.cs
@@ -10,11 +10,11 @@ public class ProjectileLauncher : MonoBehaviour
     public void fireProjectile()
     {
         GameObject projectile = Instantiate(projectilePrefab, firePoint.position, projectilePrefab.transform.rotation);
-        Vector3 origScale = projectile.transform.localScale;
+        Vector3 origScale = transform.localScale;
 
         //Flips the projectile depending on the direction the user's facing.
         projectile.transform.localScale = new Vector3(
-            origScale.x * (transform.localScale.x > 0 ? 1 : -1),
+            origScale.x * transform.localScale.x > 0 ? 1 : -1,
             origScale.y,
             origScale.z
             );
