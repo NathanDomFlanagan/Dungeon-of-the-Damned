013ca4047f6b6e56c7cf6462d4dfe2ddf1c04dca
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-22 18:50:12 +1200

    dsa

--- Assets/Scripts/PrefabDatabase.cs
+++ Assets/Scripts/PrefabDatabase.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 [CreateAssetMenu]
 public class PrefabDatabase : ScriptableObject
 {
-    public GameObject[] characterPrefab;
+    public PrefabSelect[] characterPrefab;
     public int prefabIndex
     {
         get { return characterPrefab.Length; }
     }
 
-    public GameObject getPrefab(int index)
+    public PrefabSelect getPrefab(int index)
     {
         if(index > characterPrefab.Length)
         {
-            return 0;
+            return characterPrefab[0];
         } else
         {
             return characterPrefab[index]; 
---
+++ Assets/Scripts/PrefabSelect.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrefabSelect
+{
+    public string prefabName;
+    public GameObject prefab;
+}
