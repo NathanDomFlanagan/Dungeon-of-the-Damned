37f16c5988fe47ca0923d33d00d21700cfd581a0
Nathan Flanagan <102491889+NathanDomFlanagan@users.noreply.github.com>
2023-05-23 22:57:37 +1200

    Updated sound

--- Assets/GameSound.cs
+++ Assets/GameSound.cs
@@ -6,9 +6,12 @@ public class GameSound : MonoBehaviour
 {
     public AudioClip attackSound;
     public AudioClip jumpSound;
+    public AudioClip playerInjureEnemySound;
+    public AudioClip enemyInjurePlayerSound;
     public float attackDelay = 1.0f;
     public float attackVolume = 1.0f;
     public float jumpVolume = 1.0f;
+    public float collisionVolume = 1.0f;
     private AudioSource audioSource;
     private bool canAttack = true;
 
@@ -41,5 +44,20 @@ public class GameSound : MonoBehaviour
         audioSource.PlayOneShot(attackSound);
         yield return new WaitForSeconds(attackDelay);
         canAttack = true;
+    }
+
+    public void PlayAttackSound()
+    {
+        audioSource.PlayOneShot(attackSound, attackVolume);
+    }
+
+    public void PlayPlayerInjureEnemySound()
+    {
+        audioSource.PlayOneShot(playerInjureEnemySound, collisionVolume);
+    }
+
+    public void PlayEnemyInjurePlayerSound()
+    {
+        audioSource.PlayOneShot(enemyInjurePlayerSound, collisionVolume);
     }
 }
