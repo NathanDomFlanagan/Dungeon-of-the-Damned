5f650c95f5708bb4ad5b22367596be6968a5f398
NicholasJorgensenAUT <gvx0569@autuni.ac.nz>
2023-05-25 20:43:33 +1200

    Squashed commit of the following:
    commit a75a301804ab15d0428904d15324d035ebd286c7
    Author: NicholasJorgensenAUT <gvx0569@autuni.ac.nz>
    Date:   Thu May 11 21:10:26 2023 +1200
    
        Player Continuity and Camera Seeking
    
        The player will only be created in new scenes if there is not one already existing. The Cinemachine has been updated with a script to seek out hte plaeyr object and change their follow and lookat to that gameobjects transform

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    private CinemachineVirtualCamera vcam;
+    private Transform rbTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        rbTransform = player.transform;
+        vcam.LookAt = rbTransform;
+        vcam.Follow = rbTransform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
