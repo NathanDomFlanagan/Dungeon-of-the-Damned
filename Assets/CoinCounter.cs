fc7001936d7b56eea8b10b7a57b52acb604d79be
lucae3101 <114625986+lucae3101@users.noreply.github.com>
2023-05-26 18:39:53 +1200

    Readded Files from CoinSystemBranch
    Manually added files from coin system branch

---
+++ Assets/CoinCounter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Diagnostics;
+
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+
+    public TMP_Text coinText;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+       
+    }
+    void Start()
+    {
+        //Sets string to current coin count
+        //PlayerPrefs.SetString("cointext",PlayerPrefs.GetInt("coins").ToString());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets string to current coin count
+        coinText.text = PlayerPrefs.GetInt("coins").ToString();
+    }
+    public void AddCoins(int x)
+    {
+        //Adds coins to coin count
+        PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins")+x);
+
+        //PlayerPrefs.SetString("cointext",PlayerPrefs.GetInt("coins").ToString());
+        //Updates text to display new coin count
+        coinText.text = PlayerPrefs.GetInt("coins").ToString();
+    }
+}
