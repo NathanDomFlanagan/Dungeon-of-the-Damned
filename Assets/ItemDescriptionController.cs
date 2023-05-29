337bff7de37a19ddbb6a48e6c1972bdec0661d36
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-05-29 22:33:06 +1200

    Implemented mouse on hover
    Now the item icon, name, and description is displayed on the right side. Need to finish player stats and equip afterwards

--- Assets/ItemDescriptionController.cs
+++ Assets/ItemDescriptionController.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemDescriptionController : MonoBehaviour
 {
+    public Transform itemImage;
+    public Transform itemName;
+    public Transform itemDesc;
 
+    private ItemInventoryController im;
+
+    void Awake()
+    {
+        itemImage = GameObject.Find("Description/ItemImage").transform;
+        itemName = GameObject.Find("Description/ItemName").transform;
+        itemDesc = GameObject.Find("Description/ItemDesc").transform;
+        im = GetComponent<ItemInventoryController>();
+
+    }
+    public void setDescription()
+    {
+
+        itemImage.GetComponent<Image>().sprite = im.item.itemIcon;
+        itemName.GetComponent<TMP_Text>().text = im.item.itemName;
+        itemDesc.GetComponent<TMP_Text>().text = im.item.description;
+
+    }
 }
