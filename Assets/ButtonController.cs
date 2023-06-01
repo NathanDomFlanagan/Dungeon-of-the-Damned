54fbd6f901c1933800c97d0eac3561d22c0e5fc4
Nathan Flanagan <102491889+NathanDomFlanagan@users.noreply.github.com>
2023-06-01 13:34:31 +1200

    Fixed button and added another door

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -5,17 +5,17 @@ public class ButtonController : MonoBehaviour
     public GameObject objectToDisappear;
     public Color triggeredColor; // New variable to hold the color of the button when triggered
 
-    private Renderer buttonRenderer; // Reference to the button object's Renderer component
+    private SpriteRenderer buttonRenderer; // Reference to the button object's SpriteRenderer component
     private Color originalColor; // Variable to store the original color of the button
     private bool isTriggered; // Variable to track if the button has been triggered
 
     private void Start()
     {
-        // Get the Renderer component from the button object
-        buttonRenderer = GetComponent<Renderer>();
+        // Get the SpriteRenderer component from the button object
+        buttonRenderer = GetComponent<SpriteRenderer>();
 
         // Store the original color of the button
-        originalColor = buttonRenderer.material.color;
+        originalColor = buttonRenderer.color;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,10 +26,11 @@ public class ButtonController : MonoBehaviour
             objectToDisappear.SetActive(false);
 
             // Change the color of the button to the triggered color
-            buttonRenderer.material.color = triggeredColor;
+            buttonRenderer.color = triggeredColor;
 
             // Set the button as triggered
             isTriggered = true;
         }
     }
 }
+
