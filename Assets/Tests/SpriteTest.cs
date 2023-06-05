7926ac8ea39549da11598f63345d8529cf5c73e9
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-06-06 01:19:23 +1200

    Temp

---
+++ Assets/Tests/SpriteTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class SpriteTest
+{
+    [Test]
+    public void SimpleSpriteTest()
+    {
+        //Initializing variables
+        CharacterSpriteDatabse obj = new CharacterSpriteDatabse();
+    }   
+}
