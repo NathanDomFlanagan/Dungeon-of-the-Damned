97b0efe80d303079168b8697ad2f3d41e462e097
chrislau0304 <christopherlau16@gmail.com>
2023-06-05 21:11:38 +1200

    redo

--- Assets/Tests/Editor/ShopTestScript.cs
+++ Assets/Tests/Editor/ShopTestScript.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using NUnit.Framework;
-//using ShopManager;
+using UnityEngine;
+using UnityEngine.TestTools;
 
-public class ShopTests
+public class ShopTestScript
 {
     [Test]
     public void DeductCoinsOnPurchase()
