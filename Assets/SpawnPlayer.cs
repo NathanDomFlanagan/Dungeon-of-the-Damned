eb8ff5709782049e48f1fe8f677f0b46cbc8ad8b
RainierJared <116169371+RainierJared@users.noreply.github.com>
2023-06-06 02:28:01 +1200

    Minor bug fixes
    Fixed the error where the Archer was using the knight's Animator Controller and was using the knight in PlayerModel

--- Assets/SpawnPlayer.cs
+++ Assets/SpawnPlayer.cs
@@ -6,16 +6,12 @@ using Cinemachine;
 public class SpawnPlayer : MonoBehaviour
 {
     public Transform spawnPoint;
-    public CinemachineVirtualCamera camera;
     // Start is called before the first frame update
     void Awake()
     {
         Debug.Log(PlayerPrefs.GetString("className"));
         Instantiate(Resources.Load(PlayerPrefs.GetString("className")),spawnPoint.position, spawnPoint.rotation);
         var targets = GameObject.FindGameObjectsWithTag("Player");
-
-        CameraController camcon = camera.GetComponent<CameraController>();
-        camcon.Find();
     }
 
 }
