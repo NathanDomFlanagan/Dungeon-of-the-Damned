c3cfa7700b7e9086a91ebcd183f1ca7ae65643e7
chrislau0304 <christopherlau16@gmail.com>
2023-06-05 19:15:09 +1200

    added test script
    ShopTest added

---
+++ Assets/Tests/ShopTest.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+//using ShopManager;
+
+public class ShopTests
+{
+    [Test]
+    public void DeductCoinsOnPurchase()
+    {
+        // Create a mock shop and player inventory
+        ShopManager shopManager = new ShopManager();
+        PlayerInventory playerInventory = new PlayerInventory();
+
+        // Set up test data
+        int initialCoins = 100;
+        int itemCost = 50;
+        shopManager.coinCounter.SetCoins(initialCoins);
+
+        // Purchase an item
+        shopManager.PurchaseItem(0); // Assuming item 0 is selected for testing
+
+        // Verify the coins are correctly deducted
+        int expectedCoins = initialCoins - itemCost;
+        int actualCoins = shopManager.coinCounter.GetCoins();
+        Assert.AreEqual(expectedCoins, actualCoins, "Coins were not deducted correctly on purchase.");
+    }
+}
